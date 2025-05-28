82b1807ebca69ed0cd3e7b0239be45be5e07b3fd
Valery-Holovina <valery.holovina09@gmail.com>
2025-05-28 15:43:44 +0300

    Initial Commit

---
+++ ToDoList2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoList2025
+//
+//  Created by Valery on 28.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoList2025/ToDoList2025App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList2025App.swift
+//  ToDoList2025
+//
+//  Created by Valery on 28.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
